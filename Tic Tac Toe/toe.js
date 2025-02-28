4ed2adb4f6efbd72f1c42ea79c29e8949cd242ea
Piyush Purohit <workforpiyush480@gmail.com>
2025-02-28 20:40:50 +0530

    js file add

---
+++ Tic Tac Toe/toe.js
@@ -0,0 +1,151 @@
+let arr = ["PlayerA","PlayerB"]
+let turn = arr[0]
+
+ document.getElementById('b1').addEventListener("click",function(){
+   if(document.getElementById("b1").textContent !== "" ){
+    return;
+   }
+
+  if(turn === arr[0]){
+    document.getElementById("b1").textContent = "X"
+    turn = arr[1]
+  }
+  else{
+    document.getElementById("b1").textContent = "O"
+    turn = arr[0]
+  }
+})
+
+
+ document.getElementById('b2').addEventListener("click",function(){
+  if(document.getElementById("b2").textContent !== "" ){
+    return;
+   }
+  
+  if(turn === arr[0]){
+    document.getElementById("b2").textContent = "X"
+    turn = arr[1]
+  }
+  else{
+    document.getElementById("b2").textContent = "O"
+    turn = arr[0]
+  }
+})
+
+
+ document.getElementById('b3').addEventListener("click",function(){
+  if(document.getElementById("b3").textContent !== "" ){
+    return;
+   }
+  
+  if(turn === arr[0]){
+    document.getElementById("b3").textContent = "X"
+    turn = arr[1]
+  }
+  else{
+    document.getElementById("b3").textContent = "O"
+    turn = arr[0]
+  }
+})
+
+
+
+document.getElementById('b4').addEventListener("click",function(){
+  if(document.getElementById("b4").textContent !== "" ){
+    return;
+   }
+  
+  if(turn === arr[0]){
+    document.getElementById("b4").textContent = "X"
+    turn = arr[1]
+  }
+  else{
+    document.getElementById("b4").textContent = "O"
+    turn = arr[0]
+  }
+})
+
+
+
+ document.getElementById('b5').addEventListener("click",function(){
+  if(document.getElementById("b5").textContent !== "" ){
+    return;
+   }
+  
+  if(turn === arr[0]){
+    document.getElementById("b5").textContent = "X"
+    turn = arr[1]
+  }
+  else{
+    document.getElementById("b5").textContent = "O"
+    turn = arr[0]
+  }
+})
+
+
+
+ document.getElementById('b6').addEventListener("click",function(){
+  if(document.getElementById("b6").textContent !== "" ){
+    return;
+   }
+  
+  if(turn === arr[0]){
+    document.getElementById("b6").textContent = "X"
+    turn = arr[1]
+  }
+  else{
+    document.getElementById("b6").textContent = "O"
+    turn = arr[0]
+  }
+})
+
+
+
+ document.getElementById('b7').addEventListener("click",function(){
+  if(document.getElementById("b7").textContent !== "" ){
+    return;
+   }
+  
+  if(turn === arr[0]){
+    document.getElementById("b7").textContent = "X"
+    turn = arr[1]
+  }
+  else{
+    document.getElementById("b7").textContent = "O"
+    turn = arr[0]
+  }
+})
+
+
+
+ document.getElementById('b8').addEventListener("click",function(){
+  if(document.getElementById("b8").textContent !== "" ){
+    return;
+   }
+  
+  if(turn === arr[0]){
+    document.getElementById("b8").textContent = "X"
+    turn = arr[1]
+  }
+  else{
+    document.getElementById("b8").textContent = "O"
+    turn = arr[0]
+  }
+})
+
+
+
+ document.getElementById('b9').addEventListener("click",function(){
+  if(document.getElementById("b9").textContent !== "" ){
+    return;
+   }
+  
+  if(turn === arr[0]){
+    document.getElementById("b9").textContent = "X"
+    turn = arr[1]
+  }
+  else{
+    document.getElementById("b9").textContent = "O"
+    turn = arr[0]
+  }
+})
